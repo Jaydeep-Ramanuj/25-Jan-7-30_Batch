8fc2702ccd625d94e40c563ebc1ab4c9a036a288
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-01-27 08:44:41 +0530

    Js Lecture 2

---
+++ data-types.js
@@ -0,0 +1,19 @@
+// String
+'aaahncdjjsd4356';
+// number
+1234.34645;
+// boolean
+true / false;
+
+// Null
+let a = null;
+// Undefined
+let b = undefined;
+
+// var z = 10;
+let z = 20;
+
+a = 10;
+a = '10';
+a = b;
+a = {};
